1469dc44fcecbc1260ca19e2ce0a558f16f867f7
doseeare <k7karimov@gmail.com>
2020-12-21 15:17:00 +0600

    Initial commit

---
+++ app/src/main/java/rd/doseeare/paymepls/MainActivity.kt
@@ -0,0 +1,11 @@
+package rd.doseeare.paymepls
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
